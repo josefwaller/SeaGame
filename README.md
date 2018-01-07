db3aaa05f96a51ccb25ab830cc0c88c446a3f62d
Josef Waller <josef@siriusapplications.com>
2018-01-07 15:03:15 -0800

    Add SFML

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ SeaGame
 =======
 
 A game where you are a ship and sail around the sea. WIP.
+
+To run, have SFML 2.4.2 64 bit in the `SFML` folder.
--- SeaGame/src/main.cpp
+++ SeaGame/src/main.cpp
@@ -1,4 +1,18 @@
+#include <SFML/Graphics.hpp>
+
 int main()
 {
+	sf::RenderWindow w(sf::VideoMode(800, 600), "Sea Game");
+	while (w.isOpen())
+	{
+		sf::Event e;
+		while (w.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed)
+				w.close();
+		}
+		w.clear();
+		w.display();
+	}
 	return 0;
 }
