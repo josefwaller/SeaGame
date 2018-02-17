72df54f78828b1adf1b09afe851e003653670178
Josef Waller <josef@siriusapplications.com>
2018-02-17 13:18:10 -0800

    Make islands render properly
    Islands now use the proper tiles on the border. Split the z-index tiles
    into land tiles and sea tiles so that sea tiles can be drawn below land
    tiles.

--- SeaGame/src/GameMap.cpp
+++ SeaGame/src/GameMap.cpp
@@ -1,38 +1,6 @@
 #include "GameMap.h"
 #include "ResourceManager.h"
 
-// Names of the various different types of tiles
-std::map<GameMap::TileType, sf::Sprite> GameMap::tileSprites = []() {
-	// Get the names of the sprites to load
-	std::map<TileType, std::string> spriteNames =
-	{
-		{
-			TileType::Sea,
-			"sea"
-		},
-		{
-			TileType::Sand,
-			"sand"
-		},
-		{
-			TileType::Grass,
-			"grass"
-		}
-	};
-	std::map<TileType, sf::Sprite> toReturn;
-	// Set sprite values
-	for (auto p : spriteNames) {
-		// Create new sprite
-		sf::Sprite s;
-		// Set attributes
-		auto spriteInfo = ResourceManager::get()->getSpriteInfo("tiles", p.second);
-		s.setTexture(*spriteInfo.first);
-		s.setTextureRect(spriteInfo.second);
-		// Add to tileSprites
-		toReturn.insert({ p.first, s });
-	}
-	return toReturn;
-}();
 GameMap::GameMap()
 {
 	// Add tiles
@@ -40,11 +8,8 @@ GameMap::GameMap()
 		this->tiles.push_back({});
 		for (auto y = 0; y < 10; y++) {
 			// Add test island in the middle
-			if (x < 7 && x > 3 && y < 7 && y > 3)
-				if (x == 5 && y == 5)
-					this->tiles[x].push_back(TileType::Grass);
-				else
-					this->tiles[x].push_back(TileType::Sand);
+			if (x < 7 && x > 3 && y < 9 && y > 3)
+				this->tiles[x].push_back(TileType::Land);
 			else
 				this->tiles[x].push_back(TileType::Sea);
 		}
@@ -57,12 +22,71 @@ void GameMap::render(RenderManager& r)
 	{
 		for (size_t y = 0; y < this->tiles[x].size(); y++)
 		{
-			sf::Sprite s = tileSprites[this->tiles[x][y]];
-			s.setPosition(
-				x * (float)s.getTextureRect().width,
-				y * (float)s.getTextureRect().height
-			);
-			r.add(s, RenderManager::INDEX_TILES);
+			// First draw sea under it
+			sf::Sprite seaSprite = ResourceManager::get()->getSprite("tiles", "sea-sea-sea-sea", false);
+			seaSprite.setPosition({ 64 * (float)x, 64 * (float)y });
+			r.add(seaSprite, RenderManager::INDEX_SEA_TILES);
+			// Draw the land if there is any
+			if (this->tiles[x][y] == TileType::Land) {
+				// Create a vector of the string name of the tile needed
+				std::vector<std::string> tileStringParts;
+				// Get the types of the tiles around it
+				std::vector<int> indexes = { -1, 0, 1, 0 };
+				for (size_t i = 0; i < indexes.size(); i++) {
+					size_t yOff = indexes[(size_t)i];
+					size_t xOff = indexes[(size_t)(i + 1) % indexes.size()];
+					// Check tile is on map
+					if (x + xOff >= 0 && x + xOff < this->tiles.size()) {
+						if (y + yOff >= 0 && y + yOff < this->tiles[x].size()) {
+							// Add string representation of edge between these two tiles
+							// e.g. "sea", "land", "shore"
+							tileStringParts.push_back(this->getEdgeType({ x, y }, { x + xOff, y + yOff }));
+							continue;
+						}
+					}
+					// Assume edge is sea if it is not on the map
+					tileStringParts.push_back("sea");
+				}
+				// Add dashes between string
+				std::string tileString = tileStringParts[0];
+				for (size_t i = 1; i < tileStringParts.size(); i++) {
+					tileString += "-" + tileStringParts[i];
+				}
+				// Get the tile
+				sf::Sprite s = ResourceManager::get()->getSprite("tiles", tileString, false);
+				s.setPosition(
+					x * (float)s.getTextureRect().width,
+					y * (float)s.getTextureRect().height
+				);
+				// Add it to be drawn
+				r.add(s, RenderManager::INDEX_LAND_TILES);
+			}
 		}
 	}
+}
+std::string GameMap::getEdgeType(sf::Vector2<size_t> one, sf::Vector2<size_t> two)
+{
+	TileType tOne = this->tiles[one.x][one.y];
+	TileType tTwo = this->tiles[two.x][two.y];
+	if (tOne == TileType::Sea || tTwo == TileType::Sea) {
+		return "sea";
+	} 
+	else {
+		if (this->hasSeaAround(one) && this->hasSeaAround(two))
+			return "shore";
+		return "land";
+	}
+}
+bool GameMap::hasSeaAround(sf::Vector2<size_t> indexes) {
+	std::vector<int> offsets = { -1, 0, 1 };
+
+	for (int x : offsets) {
+		for (int y : offsets) {
+			if (x != 0 || y != 0) {
+				if (this->tiles[indexes.x + x][indexes.y + y] == TileType::Sea)
+					return true;
+			}
+		}
+	}
+	return false;
 }
--- SeaGame/src/RenderManager.h
+++ SeaGame/src/RenderManager.h
@@ -20,19 +20,21 @@ public:
 	void reset();
 	// Z indexes to draw things on
 	// For sea tiles, land tiles, etc
-	const static size_t INDEX_TILES = 0;
+	const static size_t INDEX_SEA_TILES = 0;
+	// For non sea tiles
+	const static size_t INDEX_LAND_TILES = INDEX_SEA_TILES + 1;
 	// For ship hulls
-	const static size_t INDEX_HULL = 1;
+	const static size_t INDEX_HULL = INDEX_LAND_TILES + 1;
 	// For cannonballs
-	const static size_t INDEX_CANNONBALLS = 2;
+	const static size_t INDEX_CANNONBALLS = INDEX_HULL + 1;
 	// For things on the decks of ships
-	const static size_t INDEX_DECK = 3;
+	const static size_t INDEX_DECK = INDEX_CANNONBALLS + 1;
 	// For the ship sails
-	const static size_t INDEX_SAILS = 4;
+	const static size_t INDEX_SAILS = INDEX_DECK + 1;
 	// For explosions, fire, etc
-	const static size_t INDEX_EFFECT = 5;
+	const static size_t INDEX_EFFECT = INDEX_SAILS + 1;
 	// For debug information, hitboxes, etc
-	const static size_t INDEX_DEBUG = 6;
+	const static size_t INDEX_DEBUG = INDEX_EFFECT + 1;
 private:
 	std::vector<std::vector<std::shared_ptr<sf::Drawable>>> sprites;
 };
