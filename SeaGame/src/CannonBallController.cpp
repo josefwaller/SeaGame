e78d49053ea93a6a78a3b79f09bea245d767815d
Josef Waller <josef@siriusapplications.com>
2018-04-01 10:13:49 -0700

    Make cannonballs have a max distance

--- SeaGame/src/CannonBallController.cpp
+++ SeaGame/src/CannonBallController.cpp
@@ -3,17 +3,26 @@
 #include "PhysicsComponent.h"
 
 const float CannonBallController::SPEED = 300.0f;
+const float CannonBallController::MAX_DISTANCE = 500.0f;
 CannonBallController::CannonBallController(std::weak_ptr<Entity> parent, float angle, std::weak_ptr<Entity> spawner)
 	: ControllerComponent(parent)
 {
 	this->spawner = spawner;
 	this->angle = angle;
+	this->distance = 0;
 }
 
 void CannonBallController::update(float delta)
 {
+	// Move the cannonball
 	auto physics = this->getParent()->physics;
 	physics->setVelocity({ SPEED * cos(this->angle), SPEED * sin(this->angle) });
+	// Check if it has gone far enough and should be destroyed
+	this->distance += delta * SPEED;
+	if (this->distance >= MAX_DISTANCE) {
+		this->getParent()->game->removeEntity(this->getParent());
+	}
+
 }
 void CannonBallController::onCollision(std::weak_ptr<Entity> other)
 {
--- SeaGame/src/CannonBallController.h
+++ SeaGame/src/CannonBallController.h
@@ -4,13 +4,18 @@
 class CannonBallController : public ControllerComponent
 {
 public:
+	// How far each cannonball goes before being destroyed
+	static const float MAX_DISTANCE;
+	// Speed at which cannonballs move
+	static const float SPEED;
+
 	CannonBallController(std::weak_ptr<Entity> parent, float angle, std::weak_ptr<Entity> spawner);
 	virtual void update(float delta) override;
 	virtual void onCollision(std::weak_ptr<Entity> spawner) override;
-	// Speed at which cannonballs move
-	static const float SPEED;
 private:
 	// Ship which shot the cannonball
 	std::weak_ptr<Entity> spawner;
 	float angle;
+	// Distance the cannonball has gone
+	float distance;
 };
