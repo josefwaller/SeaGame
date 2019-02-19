9db2bf3b89c37eb3675352fdb86f90fd9b9f441b
Josef Waller <josef@siriusapplications.com>
2019-02-19 10:48:30 -0800

    Add gui for MininBaseController

--- SeaGame/src/MiningBaseController.cpp
+++ SeaGame/src/MiningBaseController.cpp
@@ -6,12 +6,14 @@
 const float MiningBaseController::GENERATION_DELAY = 2000.0f;
 MiningBaseController::MiningBaseController(GameResource res) : BaseController() {
 	this->res = res;
+	this->progressGui = tgui::ProgressBar::create();
 }
 void MiningBaseController::update(float delta) {
 	if (this->resourceClock.getElapsedTime().asMilliseconds() > MiningBaseController::GENERATION_DELAY) {
 		this->getParent().lock()->components.inventory->addItems(this->res, 1);
 		this->resourceClock.restart();
 	}
+	this->progressGui->setValue(100 * this->resourceClock.getElapsedTime().asMilliseconds() / GENERATION_DELAY);
 }
 GameResource MiningBaseController::getResource() {
 	return this->res;
@@ -23,4 +25,15 @@ SaveData MiningBaseController::getSaveData() {
 }
 void MiningBaseController::fromSaveData(SaveData data) {
 	this->res = (GameResource)(std::stoi(data.getValue("product")));
+}
+tgui::Widget::Ptr MiningBaseController::getGui() {
+	auto panel = tgui::Panel::create();
+	// Add label
+	auto label = tgui::Label::create();
+	label->setText("Progress: ");
+	panel->add(label);
+	// Add progress bar
+	panel->add(this->progressGui);
+	this->progressGui->setPosition(tgui::bindWidth(label), 0);
+	return panel;
 }
--- SeaGame/src/MiningBaseController.h
+++ SeaGame/src/MiningBaseController.h
@@ -14,8 +14,11 @@ public:
 	GameResource getResource();
 	virtual SaveData getSaveData() override;
 	virtual void fromSaveData(SaveData data) override;
+	virtual tgui::Widget::Ptr getGui() override;
 private:
 	static const float GENERATION_DELAY;
 	GameResource res;
 	sf::Clock resourceClock;
+	// The gui of how far the generation is
+	tgui::ProgressBar::Ptr progressGui;
 };
